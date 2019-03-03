80d27e7a31a721f31ec09ae7d8e21ae13d118ef7
Tom Hutman <tom@ath92.nl>
2019-03-03 14:44:20 +0100

    a touch of color

--- main.js
+++ main.js
@@ -24,14 +24,14 @@ function draw() {
 	drawCircle(windowWidth / 2, // x
 	           windowHeight / 2, // y
 	           windowHeight / 3, // radius
-	           1000 + Math.cos(mouseX / windowWidth * 2 * Math.PI) * 500, // segments
-	           Math.cos(current / 300) * 500 + 1000, //wobbleFactor
+	           2000 + Math.cos(mouseX / windowWidth * 2 * Math.PI) * 200, // segments
+	           Math.cos(current / 30) * 500 + 1000, //wobbleFactor
 	           Math.sin(current / 1000000) + 100); // wobblePeriod
 }
 
 function drawCircle(x, y, radius, segments, wobbleFactor = 0.1, wobblePeriod = 1) {
 	for (let i = 0; i < segments; i++){
-
+		stroke(Math.tan(i/1000 + counter/100) * 255 + 100, Math.sin(i/2000 + counter/100) * 255 + 100, Math.cos(i/3000 + counter/100) * 255 + 100)
 		line(x + Math.sin(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
 			 y + Math.cos(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
 			 x + Math.sin((i / segments) * 2 * Math.PI) * (radius + Math.sin(i * wobblePeriod) * radius * wobbleFactor),
