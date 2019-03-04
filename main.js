2c2960d62b0f2a11709c600c61bf6b8155ddd27f
tomh <tom.hutman@adyen.com>
2019-03-04 23:34:15 +0100

    now this is just weird

--- main.js
+++ main.js
@@ -26,16 +26,16 @@ function draw() {
 	           windowHeight / 2, // y
 	           windowHeight / 3, // radius
 	           2000, // segments
-	           Math.cos(counter / 1000 + Math.PI) * 500 + 500, //wobbleFactor
-	           Math.sin(counter / 1000000) * 0.5 + 200); // wobblePeriod
+	           Math.cos(counter / 1000 + Math.PI) * 500 + 499, //wobbleFactor
+	           Math.sin(counter / 1000000) * 0.5); // wobblePeriod
 }
 
 function drawCircle(x, y, radius, segments, wobbleFactor = 0.1, wobblePeriod = 1) {
 	for (let i = 0; i < segments; i++){
 		stroke((Math.sin(counter % i) + 10) * 255 + 255, Math.sin(i/2000 + counter/100) * 255 + 255, Math.cos(i/3000 + counter/100) * 255 + 255)
-		line(x + Math.sin(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
-			 y + Math.cos(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
-			 x + Math.sin((i / segments) * 2 * Math.PI) * (radius + Math.sin(i * wobblePeriod) * radius * wobbleFactor),
+		line(x + Math.sin(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod * 2) * radius * wobbleFactor),
+			 y + Math.cos(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod * 3) * radius * wobbleFactor),
+			 x + Math.sin((i / segments) * 2 * Math.PI) * (radius + Math.sin(i * wobblePeriod ) * radius * wobbleFactor),
 			 y + Math.cos((i / segments) * 2 * Math.PI) * (radius + Math.sin(i * wobblePeriod) * radius * wobbleFactor));
 	}
 }
