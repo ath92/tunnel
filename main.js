e8f9dd127489912cf5b14d60585c4fbf35006863
Tom Hutman <tom@ath92.nl>
2019-03-04 22:06:25 +0100

    go deeper

--- main.js
+++ main.js
@@ -19,19 +19,20 @@ function draw() {
 	actualX += (targetX - actualX) / 20;
 	actualY += (targetY - actualY) / 20;
 	counter++;
-	const current = counter + mouseY;
+	blendMode(DARKEST);
 	background(0);
+	blendMode(ADD);
 	drawCircle(windowWidth / 2, // x
 	           windowHeight / 2, // y
 	           windowHeight / 3, // radius
-	           2000 + Math.cos(mouseX / windowWidth * 2 * Math.PI) * 200, // segments
-	           Math.cos(current / 30) * 500 + 1000, //wobbleFactor
-	           Math.sin(current / 1000000) + 100); // wobblePeriod
+	           2000, // segments
+	           Math.cos(counter / 1000 + Math.PI) * 500 + 500, //wobbleFactor
+	           Math.sin(counter / 1000000) * 0.5 + 200); // wobblePeriod
 }
 
 function drawCircle(x, y, radius, segments, wobbleFactor = 0.1, wobblePeriod = 1) {
 	for (let i = 0; i < segments; i++){
-		stroke(Math.tan(i/1000 + counter/100) * 255 + 100, Math.sin(i/2000 + counter/100) * 255 + 100, Math.cos(i/3000 + counter/100) * 255 + 100)
+		stroke((Math.sin(counter % i) + 10) * 255 + 255, Math.sin(i/2000 + counter/100) * 255 + 255, Math.cos(i/3000 + counter/100) * 255 + 255)
 		line(x + Math.sin(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
 			 y + Math.cos(((i - 1) / segments) * 2 * Math.PI) * (radius + Math.sin((i - 1) * wobblePeriod) * radius * wobbleFactor),
 			 x + Math.sin((i / segments) * 2 * Math.PI) * (radius + Math.sin(i * wobblePeriod) * radius * wobbleFactor),
